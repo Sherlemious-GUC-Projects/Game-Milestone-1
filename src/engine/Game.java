b5585c9fa2697460d20016705b86622f8603b805
Abdulrahman <abd.moh.yousef@gmail.com>
2023-03-18 21:29:40 +0200

    Created the Game Class

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,5 +1,49 @@
 package engine;
-// TODO: Implement Game class
+
+import model.characters.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Game {
+    public static ArrayList<Hero> availableHeros;
+    public static ArrayList<Hero> heros;
+    public static ArrayList<Zombie> zombies;
+    public static Cell [][] map;
+
+    public static void loadHeros(String path) throws FileNotFoundException {
+        File file = new File(path);
+
+        Scanner fileReader = new Scanner(file);
+
+        while(fileReader.hasNext()){
+            String line = fileReader.nextLine(); // read line
+            String[] data = line.split(","); // split line by comma
+            String name = data[0];
+            String type = data[1];
+            int maxHp = Integer.parseInt(data[2]);
+            int maxActions = Integer.parseInt(data[3]);
+            int attackDmg = Integer.parseInt(data[4]);
+            
+            Hero hero;
+            switch (type) {
+                case "FIGH":
+                    hero = new Fighter(name, maxHp, attackDmg, maxActions);
+                    break;
+                case "EXP":
+                    hero = new Explorer(name, maxHp, attackDmg, maxActions);
+                    break;
+                case "MED":
+                    hero = new Medic(name, maxHp, attackDmg, maxActions);
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + type);
+            }
+            availableHeros.add(hero);
+
+        }
+    }
+
 }
