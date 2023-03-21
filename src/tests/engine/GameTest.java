67368001baa163e6f7000e4c9c91e929fb5f97ad
Abdulrahman <abd.moh.yousef@gmail.com>
2023-03-21 15:17:21 +0200

    Fixed Personal Tests

--- src/tests/engine/GameTest.java
+++ src/tests/engine/GameTest.java
@@ -9,41 +9,41 @@ import static org.junit.Assert.*;
 public class GameTest {
 
     @Test
-    public void loadHeros() throws FileNotFoundException {
-        Game.availableHeros = new ArrayList<>();
-        Game.loadHeros("A:\\University\\Projects\\CS 4 Game\\Heros.csv");
-        assertEquals("Joel Miller", Game.availableHeros.get(0).getName());
+    public void loadHeroes() throws FileNotFoundException {
+        Game.availableHeroes = new ArrayList<>();
+        Game.loadHeroes("A:\\University\\Projects\\CS 4 Game\\Heros.csv");
+        assertEquals("Joel Miller", Game.availableHeroes.get(0).getName());
     }
     @Test
     public void loadHeros2() throws FileNotFoundException {
-        Game.availableHeros = new ArrayList<>();
-        Game.loadHeros("A:\\University\\Projects\\CS 4 Game\\Heros.csv");
-        assertEquals(140, Game.availableHeros.get(0).getMaxHp());
+        Game.availableHeroes = new ArrayList<>();
+        Game.loadHeroes("A:\\University\\Projects\\CS 4 Game\\Heros.csv");
+        assertEquals(140, Game.availableHeroes.get(0).getMaxHp());
     }
     @Test
     public void loadHeros3() throws FileNotFoundException {
-        Game.availableHeros = new ArrayList<>();
-        Game.loadHeros("A:\\University\\Projects\\CS 4 Game\\Heros.csv");
-        assertEquals(5, Game.availableHeros.get(0).getMaxActions());
+        Game.availableHeroes = new ArrayList<>();
+        Game.loadHeroes("A:\\University\\Projects\\CS 4 Game\\Heros.csv");
+        assertEquals(5, Game.availableHeroes.get(0).getMaxActions());
     }
     @Test
     public void loadHeros4() throws FileNotFoundException {
-        Game.availableHeros = new ArrayList<>();
-        Game.loadHeros("A:\\University\\Projects\\CS 4 Game\\Heros.csv");
-        assertEquals(30, Game.availableHeros.get(0).getAttackDmg());
+        Game.availableHeroes = new ArrayList<>();
+        Game.loadHeroes("A:\\University\\Projects\\CS 4 Game\\Heros.csv");
+        assertEquals(30, Game.availableHeroes.get(0).getAttackDmg());
     }
 
     @Test
     public void loadHeros5() throws FileNotFoundException {
-        Game.availableHeros = new ArrayList<>();
-        Game.loadHeros("A:\\University\\Projects\\CS 4 Game\\Heros.csv");
-        assertEquals("Ellie Williams", Game.availableHeros.get(1).getName());
+        Game.availableHeroes = new ArrayList<>();
+        Game.loadHeroes("A:\\University\\Projects\\CS 4 Game\\Heros.csv");
+        assertEquals("Ellie Williams", Game.availableHeroes.get(1).getName());
     }
 
     @Test
     public void loadHeros6() throws FileNotFoundException {
-        Game.availableHeros = new ArrayList<>();
-        Game.loadHeros("A:\\University\\Projects\\CS 4 Game\\Heros.csv");
-        assertEquals(110, Game.availableHeros.get(1).getMaxHp());
+        Game.availableHeroes = new ArrayList<>();
+        Game.loadHeroes("A:\\University\\Projects\\CS 4 Game\\Heros.csv");
+        assertEquals(110, Game.availableHeroes.get(1).getMaxHp());
     }
 }
